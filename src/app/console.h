f50de1a2932e3d79ada9d31b15d489d6bf762056
StarMonthStudio <61019628+StarMonthStudio@users.noreply.github.com>
2024-01-15 04:12:03 +0800

    Add files via upload

---
+++ src/app/console.h
@@ -0,0 +1,36 @@
+// Aseprite
+// Copyright (C) 2018-2022  Igara Studio S.A.
+// Copyright (C) 2001-2017  David Capello
+//
+// This program is distributed under the terms of
+// the End-User License Agreement for Aseprite.
+
+#ifndef APP_CONSOLE_H_INCLUDED
+#define APP_CONSOLE_H_INCLUDED
+#pragma once
+
+#include <exception>
+
+namespace app {
+  class Context;
+
+  class Console {
+  public:
+    Console(Context* ctx = nullptr);
+    ~Console();
+
+    void printf(const char *format, ...);
+
+    static void showException(const std::exception& e);
+    static void notifyNewDisplayConfiguration();
+
+  private:
+    class ConsoleWindow;
+
+    bool m_withUI;
+    static ConsoleWindow* m_console;
+  };
+
+} // namespace app
+
+#endif
